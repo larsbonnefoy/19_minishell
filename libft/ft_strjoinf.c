7363bf44fb604d3ab7c4415bdb18285a5a0315b4
Lars Bonnefoy <lbonnefo@fu-r8-p1.s19.be>
2023-02-17 11:06:47 +0100

    mod libft

--- libft/ft_strjoinf.c
+++ libft/ft_strjoinf.c
@@ -6,7 +6,7 @@
 /*   By: lbonnefo <lbonnefo@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 08:19:34 by lbonnefo          #+#    #+#             */
-/*   Updated: 2023/02/12 11:12:14 by lbonnefo         ###   ########.fr       */
+/*   Updated: 2023/02/17 11:02:29 by lbonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,7 @@ char	*ft_strjoinf(const char *s1, const char *s2)
 	a = -1;
 	b = -1;
 	size = ft_strlen(s1) + ft_strlen(s2);
-	str = malloc((size + 1) * sizeof(char));
-	if (str == NULL)
-	{	
-		free((char *)s1);
-		return (NULL);
-	}
+	str = ft_malloc((size + 1) * sizeof(char));
 	while (s1[++a] != '\0')
 		str[a] = (char)s1[a];
 	free((char *)s1);
--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: hdelmas <hdelmas@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 11:54:57 by hdelmas           #+#    #+#             */
-/*   Updated: 2022/10/14 10:01:09 by hdelmas          ###   ########.fr       */
+/*   Updated: 2023/02/17 11:02:05 by lbonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	while (start < ft_strlen(s)
 		&& s[start + len_to_check] && len_to_check < len)
 		len_to_check++;
-	res = malloc(sizeof(char) * (len_to_check + 1));
-	if (!res)
-		return (NULL);
+	res = ft_malloc(sizeof(char) * (len_to_check + 1));
 	if (start < ft_strlen(s))
 	{
 		while (s[start + i] && i < len_to_check)
