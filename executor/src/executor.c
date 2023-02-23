53421885a56852ef8c865290618fd393e457fda6
Lars Bonnefoy <lars.bonnefoy@hotmail.com>
2023-02-23 21:54:06 +0100

    added comm + struct

--- executor/src/executor.c
+++ executor/src/executor.c
@@ -12,8 +12,8 @@
 
 #include"../../src/minishell.h"
 
-void print_input_cmd_line(char **av);
-void	exec_last_cmd(t_simple_cmds *cmd, char **env, int fd[2], int nbr_pipe);
+void 	yeet_child(int *fd, int fd_in, t_simple_cmds *cmd, char **env);
+void 	print_input_cmd_line(char **av);
 
 /*
  *	We take each node of the cmd table
@@ -26,55 +26,46 @@ void executor(t_simple_cmds *cmd, char **env)
 {
 	int		fd[2];
 	int		pid;
-	int		old_fd;
-	int		nbr_pipe;
+	int		in_fd;
 
-	if (pipe(fd) == -1)
-		return ;
 	print_input_cmd_line(cmd->av);
-	nbr_pipe = 0;
+	in_fd = STDIN_FILENO;
 	while (cmd->next) //if cmd->next we have to pipe
-	{
+	{ 
+		if (pipe(fd) == -1)
+			return ;
 		printf("Parent process fd[0] = %d, fd[1] = %d\n", fd[0], fd[1]);
 		pid = fork();
 		if (pid == 0)
-		{
-			printf("	child executing %s\n", cmd->av[0]);
-			if (nbr_pipe != 0)
-				dup2(fd[0], STDIN_FILENO); 
-			dup2(fd[1], STDOUT_FILENO);
-			close(fd[0]);
-			close(fd[1]);
-			ft_execve(cmd->av, env);
-		}
-		old_fd = fd[0];
+			yeet_child(fd, in_fd, cmd, env);
+		in_fd = fd[0]; //read access of pipe will be stdin of the next pipe
 		close(fd[1]);
 		cmd = cmd->next;
-		nbr_pipe++;
 		printf("------------------------\n");
 	}
-	exec_last_cmd(cmd, env, fd, nbr_pipe);
 }
 
-void	exec_last_cmd(t_simple_cmds *cmd, char **env, int fd[2], int nbr_pipe)
+/*
+ * Handle child process by redirection the inputs and outputs
+ * Le process doit lire fd[0] sur
+ * 	-> l'entrée standard quand c'est la permière fois
+ * 	-> la sortie de pipe précédent les fois d'aprés
+ * Le process doit ecrire fd[1] sur
+ * 	-> la sortie standard quand c'est la dernière commande
+ * 	-> sur la sortie du pipe ACTUEL
+ * dup2(fildes, fildes2)
+ * 	fildes2 points sur fildes 
+ * 		=> on peut ecrire sur le document depuis fildes ou fildes2
+ * 		c'est pour ça qu'on close a chaque fois pcq avec dup on a ouvert sur une nouvelle entree
+ */
+void 	yeet_child(int *fd, int fd_in, t_simple_cmds *cmd, char **env)
 {
-	int pid2;
-
-	printf("Parent process fd[0] = %d, fd[1] = %d\n", fd[0], fd[1]);
-	pid2 = fork();
-	if (pid2 == 0)
-	{
-		if (nbr_pipe != 0)
-			dup2(fd[0], STDIN_FILENO);
-		printf("	child process fd[0] = %d, fd[1] = %d\n", fd[0], fd[1]);
-		printf("	last child executing %s\n", cmd->av[0]);
-		close(fd[0]);
-		close(fd[1]);
-		ft_execve(cmd->av, env);
-	}
+	printf("	child executing %s\n", cmd->av[0]);
+	dup2(fd_in, STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
 	close(fd[0]);
 	close(fd[1]);
-	waitpid(pid2, NULL, 0);
+	ft_execve(cmd->av, env);
 }
 
 void print_input_cmd_line(char **av)
