0db5191a1e5b5e88ed29c9c547990651365a5c6b
Lars Bonnefoy <lars.bonnefoy@hotmail.com>
2023-03-13 11:50:39 +0100

    added out_redir chngs

--- src/executor/out_redir.c
+++ src/executor/out_redir.c
@@ -3,28 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   out_redir.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hdelmas <hdelmas@student.s19.be>           +#+  +:+       +#+        */
+/*   By: lbonnefo <lbonnefo@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 10:53:31 by lbonnefo          #+#    #+#             */
-/*   Updated: 2023/03/10 16:39:18 by hdelmas          ###   ########.fr       */
+/*   Updated: 2023/03/13 11:25:13 by lbonnefo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../Includes/executor.h"
-
 int	is_out_redir(t_lexer *redirection);
 
-int	get_out_fd(t_simple_cmds *cmd, int pipe_write)
+int get_out_fd(t_simple_cmds *cmd, int pipe_write)
 {
 	int		fd;
 	t_lexer	*redir;
 
 	redir = cmd->redirections;
 	fd = -2;
-	while (redir)
+	while(redir)
 	{
 		if (is_out_redir(redir))
 		{
+			if (fd != -2)
+				close(fd);
 			if (redir->token == D_GREATER)
 				fd = open(redir->str, O_CREAT | O_WRONLY | O_APPEND, 0644);
 			else
@@ -37,7 +38,7 @@ int	get_out_fd(t_simple_cmds *cmd, int pipe_write)
 			if (cmd->next != NULL)
 				close(pipe_write);
 		}
-		redir = redir->next;
+		redir=redir->next;
 	}
 	if (fd != -2)
 		return (fd);
@@ -60,16 +61,16 @@ int	is_out_redir(t_lexer *redirection)
  * Returns 1 for the first out_redir it finds in t_lexer redir
  * Else returns 0
  */
-int	has_outfile(t_lexer *redirections)
+int has_outfile(t_lexer *redirections)
 {
-	t_lexer	*curr_redir;
-
+	t_lexer *curr_redir;
 	curr_redir = redirections;
+
 	while (curr_redir)
 	{
 		if (is_out_redir(curr_redir) == 1)
 			return (1);
-		curr_redir = curr_redir->next;
+		curr_redir=curr_redir->next;
 	}
 	return (0);
 }
